0c2f2a3c64048e94b6f88e525a646c987b0aaed0
sosoblack <sosoblack18@gmail.com>
2021-04-08 14:47:31 +0000

    initial commit

---
+++ app/src/main/java/com/example/myapplicationhhhh/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplicationhhhh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import java.lang.Math;
+//import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView display = findViewById(R.id.textView);
+        //final Random ran;
+        //ran = new Random();
+        //.nextInt(9);
+
+        //see.setImageResource(R.drawable.image2);
+       // display.setText(String.valueOf(Math.random(7-9)));
+        //display.setText(String.valueOf(myArray));
+
+
+    }
+}
